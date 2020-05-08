0b8e063033b8e890e3e72476fc6c066cc0e93f2e
ibootbg <ibootbg@yahoo.com>
2020-05-08 13:35:57 -0700

    created the sample

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data.Entity;
+using System.Linq;
 
 namespace ZExtensionsTest
 {
@@ -20,6 +21,35 @@ namespace ZExtensionsTest
 
                 using (var ctx = new ZExtensionsContext($"Server = {DbServer}; Database = {DbName}; Trusted_Connection = True;"))
                 {
+                    // Update the first record
+                    ctx.Contacts.First().Address = "AddressMax10AddressMax20AddressMax30AddressMax40AddressMax50";
+                    try
+                    {
+                        ctx.SaveChanges();
+                        Console.WriteLine("SaveChanges succeeded (incorrect)");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("SaveChanges failed (correct)");
+                    }
+                    // this should fail, but it doesn't
+                    ctx.BulkSaveChanges();
+                    Console.WriteLine("BulkSaveChanges: updating 1 record succeeded (incorrect)");
+
+                    // Update all 1000 records
+                    foreach (var contact in ctx.Contacts)
+                    {
+                        contact.Address = "AddressMax10AddressMax20AddressMax30AddressMax40AddressMax50";
+                    }
+                    try
+                    {
+                        ctx.BulkSaveChanges(false);
+                        Console.WriteLine("BulkSaveChanges: Updating 1000 records succeeded (incorrect)");
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("BulkSaveChanges: Updating 1000 records failed (correct)");
+                    }
                 }
             }
             catch (Exception e)
